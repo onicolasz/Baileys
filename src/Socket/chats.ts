9c4ae800f9e8aa7f3d4e1c524991585127139ba0
Nícolas Barros <98993369+onicolasz@users.noreply.github.com>
2024-09-20 19:21:19 -0300

    feat: add ev.flush when receivedPendingNotifications

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -971,6 +971,7 @@ export const makeChatsSocket = (config: SocketConfig) => {
 			if(!authState.creds?.myAppStateKeyId && !config.mobile) {
 				ev.buffer()
 				needToFlushWithAppStateSync = true
+				ev.flush()
 			}
 		}
 	})
